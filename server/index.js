33bc3fbe6174d798cbb330b6e28d4ec7a2eeae21
RJ Mohammad <rjmohammad@outlook.com>
2017-03-25 18:23:55 -0400

    routes

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const path = require('path')
 const cors = require('cors')
+const routes = require('./routes.js')
 const app = express ()
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 app.use(cors())
 const port = process.env.PORT || 8000
-
+routes(app, express)
 app.use(express.static(path.join(__dirname, '/../client')))
 app.listen(port, () => {
   console.log("Listening on port: ", port)

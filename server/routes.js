7ab04852c2b47bf05527b5328c7187e0648c1ab3
RJ Mohammad <rjmohammad@outlook.com>
2017-03-25 22:44:24 -0400

    made viberate route

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,5 @@
 var bController = require('./controllers/bController')
 module.exports = function (app, express) {
     app.post('/button', bController.button)
+    app.post('/vibrate', bController.viberate)
 }

f5d01ba60fb72d80fcb9807124d960b988820959
Qinghao Zhang <qinghaozhang@yahoo.com>
2017-03-26 09:22:19 -0400

    add cover page

--- client/app/service/service.js
+++ client/app/service/service.js
@@ -1,14 +1,21 @@
 angular.module('CBSApp.services', [])
 
-.factory('Map', function(){
+.factory('Map', function($http,$q){
   var data = [[1,1,1,1,1,1,1,1],
               [1,1,1,1,1,1,1,1],
-              [1,1,1,1,1,1,1,0],
-              [1,1,1,1,1,1,1,1]
+              [1,1,1,1,1,1,0,0],
+              [1,1,1,1,1,0,1,1]
             ]
 
   var getMap = function(){
     return data
+  }
+
+  var getRate = function(){
+    /////backend
+    /////check the button matrix data when first vibration move(open door),
+    /////check the button matrix data when second vibration move(close door)
+    /////front end just request and get the rate
   }
 
   return {
@@ -26,3 +33,14 @@ angular.module('CBSApp.services', [])
 //     return resp.data;
 //   });
 // }
+
+
+// $q.all([Restaurants.getAll(), Auth.getUser()]).then(function(results) {
+//   $scope.restaurants = []
+//   $scope.favorList = results[1].favor
+//   for(i=0; i<results[0].length; i++){
+//     if($scope.favorList.includes(results[0][i].id)){
+//       $scope.restaurants.push(results[0][i])
+//     }
+//   }
+// })
